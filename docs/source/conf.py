d7d39b19e55bd2cabfcd6b65985a34b124cc3cc7
user <user@unknownee48e08d446e.attlocal.net>
2025-10-05 07:55:07 -0400

    Revert "First commit. Basic sim robot example and docs"
    This reverts commit 99f52e90d95467835f1a9f0ab822d236d1e5a52f.

---
+++ docs/source/conf.py
@@ -0,0 +1,47 @@
+# -- Project information -----------------------------------------------------
+
+project = "Navis"
+copyright = ""
+author = "EML 5808 - Spring 2025"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",     # Support for Google/Numpy-style docstrings
+    "sphinx.ext.viewcode",     # Adds "View Source" links to documentation
+    "sphinx.ext.autosummary",  # Automatically generate summary tables
+    "sphinx.ext.todo",         # Optional: allows .. todo:: directives
+    "sphinx.ext.intersphinx",  # For linking to Python or other package docs
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "shibuya"
+
+html_static_path = ["_static"]
+
+html_css_files = [
+    'custom.css',
+]
+
+# -- Autodoc and Napoleon settings -------------------------------------------
+
+autodoc_member_order = "bysource"
+autosummary_generate = True
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+# -- Intersphinx -------------------------------------------------------------
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+# -- Todo extension (optional) -----------------------------------------------
+
+todo_include_todos = True
